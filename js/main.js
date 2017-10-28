1454066138d572930d1e95639eca2489e98de070
Bruno N. Campos <brunosopmac@gmail.com>
2017-10-28 10:11:40 -0200

    Adding an image changer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 'use strict'
 
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello, world!';  
+myHeading.textContent = 'Hello, world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+
+    if (mySrc === 'images/firefox-badge-1.png') {
+        myImage.setAttribute('src', 'images/firefox-badge-2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-badge-1.png');
+    }
+}
